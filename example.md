3b8d32381e3d2ef7f038a7ca440e95ca1debf69b
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2025-06-28 22:14:39 +0000

    More features.

--- example.md
+++ example.md
@@ -1,14 +1,37 @@
 ---
 theme: default
 layout: default
+# Future feature, not implemented yet.
+# `tools.jar` doesn't come with versions past 8.
+# java:
+#   version: 11 # Supported: 8, 11, 17
 ---
 
 # Java Code Runner for Slidev
 
-```java {monaco-run}
+```java {monaco-run} {runnerOptions:{entrypoint:false, file_path:'MyClass.java'}}
+package demo;
+
+public class MyClass {
+    public static void foo() {
+        System.out.println("Foo");
+    }
+    public void bar() {
+        System.out.println("Bar");
+    }
+}
+```
+
+```java {monaco-run} {runnerOptions:{addSources:['MyClass.java']}}
+package demo;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        
+        MyClass.foo();
+        MyClass myObject = new MyClass();
+        myObject.bar();
     }
 }
 ```
--- setup/code-runners.ts
+++ setup/code-runners.ts
@@ -1,4 +1,7 @@
 import { defineCodeRunnersSetup } from '@slidev/types'
+// import { useNav } from '@slidev/client'
+
+const JAVA_DEFAULT_VERSION = 8;
 
 function loadScript(cdn: string): Promise<void> {
     return new Promise((resolve, reject) => {
@@ -10,41 +13,83 @@ function loadScript(cdn: string): Promise<void> {
     })
 }
 
+async function setupCheerpJ(): Promise<void> {
+    // console.log('Obtaining headmatter configuration.');
+    // const { slides } = useNav();
+    // // @ts-expect-error
+    // const javaVersion = slides.value[0].meta.slide.frontmatter?.java?.version 
+    //     ?? JAVA_DEFAULT_VERSION;
+    // console.log(`Using Java version ${javaVersion}.`);
+    console.log('Initializing CheerpJ...');
+    // await cheerpjInit({version: javaVersion});
+    await cheerpjInit({version: JAVA_DEFAULT_VERSION});
+    console.log('Obtaining tools.jar...');
+    cheerpOSAddStringFile("/str/tools.jar", await (await fetch('bootstrap/tools.jar')).bytes());
+    console.log('Creating output console.');
+    // Undocumented feature: CheerpJ implicitly looks for `#console` to write to.
+    output = document.createElement('div');
+    output.id = "console";
+    document.body.appendChild(output);
+    console.log('Initialization complete.')
+}
+
 await loadScript('https://cjrtnc.leaningtech.com/4.2/loader.js');
 
-console.log('Initializing CheerpJ...');
-await cheerpjInit();
-console.log('Obtaining tools.jar...');
-cheerpOSAddStringFile("/str/tools.jar", await (await fetch('bootstrap/tools.jar')).bytes());
-console.log('Creating output console.');
-// Undocumented feature: CheerpJ implicitly looks for `#console` to write to.
-const output = document.createElement('div');
-output.id = "console";
-document.body.appendChild(output);
-console.log('Initialization complete.')
+let output: HTMLDivElement;
+await setupCheerpJ();
+
+async function compileAndRunMain(
+    filePath: string, fileContents: string, additionalSources: string[]
+) {
+    output.innerText = '';
+    console.log('Started compiling.');
+    let processCode = await cheerpjRunMain(
+        "com.sun.tools.javac.Main", 
+        "/str/tools.jar:/files/", 
+        `/str/${filePath}`, 
+        ...additionalSources.map((src) => `/str/${src}`), 
+        "-d", 
+        "/files/", 
+        '-Xlint'
+    );
+    console.log('Finished compiling.');
+    if (processCode == 0) {
+        output.innerText = '';
+        const classNameFull = deriveMainClass(filePath, fileContents);
+        console.log(`Started process ${classNameFull}.`);
+        processCode = await cheerpjRunMain(classNameFull, "/str/tools.jar:/files/");
+        console.log('Finished process.');
+        return {text: output.innerText}
+    }
+    return {text: output.innerText}
+}
 
 export default defineCodeRunnersSetup(() => {
     return {
         async java(code, ctx) {
-            cheerpOSAddStringFile("/str/Main.java", code);
-            output.innerText = '';
-            console.log('Started compiling.');
-            let processCode = await cheerpjRunMain("com.sun.tools.javac.Main", "/str/tools.jar:/files/", "/str/Main.java", "-d", "/files/", '-Xlint');
-            console.log('Finished compiling.');
-            if (processCode == 0) {
-                output.innerText = '';
-                console.log('Started process.');
-                processCode = await cheerpjRunMain("Main", "/str/tools.jar:/files/");
-                console.log('Finished process.');
-                return {text: output.innerText}
+            const isEntrypoint = (ctx.options.entrypoint ?? true) as boolean;
+            let filePath = ctx.options['file_path'];
+            const additionalSources = (ctx.options.addSources ?? []) as string[];
+            if (!isEntrypoint && filePath === undefined) {
+                return {text:'Must specify file path.'};
             }
-            return {text: output.innerText}
+            filePath = filePath ?? 'Main.java';
+            cheerpOSAddStringFile(`/str/${filePath}`, code);
+            return isEntrypoint 
+                ? await compileAndRunMain(filePath as string, code, additionalSources) 
+                : {text: "File added."};
         }
     }
 })
 
-// Should add:
-// - Get stdout. (Implicitly looks for console. Create a dummy one and then delete it.)
-// - Define Java version.
-// - Way to define entrypoint or just add to fs.
-// - Way to define package name for entrypoint.
+// Reference: https://github.com/leaningtech/javafiddle/blob/main/src/lib/CheerpJ.svelte#L44
+function deriveMainClass(filePath: string, fileContents: string) {
+    const className = filePath.split('/').pop()!.replace('.java', '');
+    const match = fileContents.match(/package\s+(.+);/);
+    if (match && match.length > 1) {
+        const packageName = match[1];
+        return `${packageName}.${className}`;
+    } else {
+        return className;
+    }
+}
